30dc086478395e9646c95f17760aad10ad1638e6
robertpd <robertofdooley@gmail.com>
2012-05-09 18:38:18 +0100

    text output or correlates

---
+++ src/com/twitter/corpus/demo/CoSetReader.java
@@ -0,0 +1,61 @@
+package com.twitter.corpus.demo;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+import org.apache.lucene.search.TermsFilter;
+
+import com.google.common.collect.HashBiMap;
+import com.twitter.corpus.types.CoWeight;
+
+public class CoSetReader {
+	private static HashMap<Integer, ArrayList<CoWeight>> coSetMapArray;// = new HashMap<Integer, ArrayList<CoWeight>>();
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args){
+
+
+		File cosetFile = new File("/home/dock/Documents/IR/DataSets/lintool-twitter-corpus-tools-d604184/tweetIndex/coset11k.ser");
+		try{
+			ObjectInputStream docTermMapois = new ObjectInputStream(new FileInputStream(cosetFile));
+			coSetMapArray = (HashMap<Integer, ArrayList<CoWeight>>) docTermMapois.readObject();
+			docTermMapois.close();
+		}
+		catch(Exception ex){}
+		
+		File termBiMapFile = new File("/home/dock/Documents/IR/DataSets/lintool-twitter-corpus-tools-d604184/tweetIndex/termBiMap11k.ser");
+		HashBiMap<String,Integer> termBimap;// = HashBiMap.create();
+		try{
+			ObjectInputStream docTermMapois = new ObjectInputStream(new FileInputStream(termBiMapFile));
+			termBimap = (HashBiMap<String,Integer>) docTermMapois.readObject();
+			docTermMapois.close();
+		
+		BufferedWriter out = new BufferedWriter(new FileWriter("/home/dock/Documents/IR/DataSets/lintool-twitter-corpus-tools-d604184/tweetIndex/coset11k_0_3.txt"));
+		Set<Integer> keyset = coSetMapArray.keySet();
+		if((coSetMapArray != null )&& (termBimap != null)){
+			for(Integer c : keyset){
+				ArrayList<CoWeight> cwList = coSetMapArray.get(c);
+				StringBuffer sb = new StringBuffer();
+				sb.append(termBimap.inverse().get(c)  + " { ");
+				boolean createLine=false;
+				for(CoWeight cw: cwList){
+					if(cw.correlate > 0.3){
+						createLine = true;
+						sb.append(termBimap.inverse().get(cw.termId)+ ": " + cw.correlate + ", ");						
+					}
+				}
+				if(createLine){
+					out.write(sb.append("\n").toString());
+				}
+			}
+		}
+	}catch(Exception ex){
+		System.out.print("asd");
+	}
+	}
+}
